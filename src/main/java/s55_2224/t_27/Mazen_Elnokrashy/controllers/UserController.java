10079451563cd4c5d93aa7f40267b1c46be9192e
SpyThunder <mizonok182@gmail.com>
2026-03-14 20:39:44 +0200

    feat: wire GET /users/{id}/notes after notes merge

--- src/main/java/s55_2224/t_27/Mazen_Elnokrashy/controllers/UserController.java
+++ src/main/java/s55_2224/t_27/Mazen_Elnokrashy/controllers/UserController.java
@@ -2,18 +2,27 @@ package s55_2224.t_27.Mazen_Elnokrashy.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import s55_2224.t_27.Mazen_Elnokrashy.models.Note;
+import s55_2224.t_27.Mazen_Elnokrashy.services.NoteService;
 import s55_2224.t_27.Mazen_Elnokrashy.services.UserService;
 import s55_2224.t_27.Mazen_Elnokrashy.models.User;
-
+import s55_2224.t_27.Mazen_Elnokrashy.services.NoteService;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 class UserController {
+    private final NoteService noteService;
     private final UserService userService;
-    public UserController(UserService userService) {
+    UserController(UserService userService, NoteService noteService) {
         this.userService = userService;
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/{id}/notes")
+    public List<Note> getNotesByUserId(@PathVariable String id) {
+        return noteService.getNotesByUserId(id);
     }
 
     @GetMapping("/")
