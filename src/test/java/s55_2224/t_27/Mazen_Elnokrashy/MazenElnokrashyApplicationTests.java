580914180a1f931cf3b9ffb34c9f02bd6a50fc15
SpyThunder <mizonok182@gmail.com>
2026-03-14 15:12:05 +0200

    Initial commit: project setup

---
+++ src/test/java/s55_2224/t_27/Mazen_Elnokrashy/MazenElnokrashyApplicationTests.java
@@ -0,0 +1,13 @@
+package s55_2224.t_27.Mazen_Elnokrashy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazenElnokrashyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
