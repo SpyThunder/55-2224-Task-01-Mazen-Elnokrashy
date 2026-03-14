a0c688c12a3af4f8ccbc13163a59bcf23fa0c543
SpyThunder <mizonok182@gmail.com>
2026-03-14 15:12:05 +0200

    Start

---
+++ src/main/java/s55_2224/t_27/Mazen_Elnokrashy/MazenElnokrashyApplication.java
@@ -0,0 +1,13 @@
+package s55_2224.t_27.Mazen_Elnokrashy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazenElnokrashyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazenElnokrashyApplication.class, args);
+	}
+
+}
